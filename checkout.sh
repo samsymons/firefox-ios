08aeeea8521c723c325e48a4678fa5f085bd3d89
Stephan Leroux <sleroux@mozilla.com>
2015-07-30 09:51:49 -0400

    Bug 1187161 - CGContextDrawImage, CGBitmapContextCreate errors in SDWebImage

--- checkout.sh
+++ checkout.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env sh
 carthage checkout --no-use-binaries
+
+# WORKAROUND - SDWebImage has issues decoding images at the moment. Override the decoder with a version that fixes the issue.
+# Github issue: https://github.com/rs/SDWebImage/issues/1203
+echo "Temporarily patch SDWebImage checkout to resolve Issue 1203 - Decoding image issues"
+echo "See https://bugzilla.mozilla.org/show_bug.cgi?id=1187161"
+cp patches/SDWebImageDecoder.m.1203fix Carthage/Checkouts/SDWebImage/SDWebImage/SDWebImageDecoder.m
